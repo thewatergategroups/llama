05a0f246f4b1fef85dca4e076a55f0236a5210c4
root <root@Laptop-P1SX9DV.localdomain>
2023-07-16 20:17:24 +0100

    fix ruff

--- alpaca/__init__.py
+++ alpaca/__init__.py
@@ -3,3 +3,8 @@ exporting function we want outwardly accessible
 """
 from .subscriber import subscribe
 from .consts import GLOBAL_SETTINGS
+
+__all__ = [
+    "subscribe",
+    "GLOBAL_SETTINGS",
+]
