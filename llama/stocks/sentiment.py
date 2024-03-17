8313a8b120562deaba06142c37b2d32c577bdacc
Boris <bobiboychev@gmail.com>
2024-03-17 16:29:50 +0000

    normaize twitter data from df

--- llama/stocks/sentiment.py
+++ llama/stocks/sentiment.py
@@ -16,13 +16,13 @@ stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(formatter)
 stdout_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-file_handler = logging.FileHandler('/home/borisb/projects/llama/gol/gvk.log')
+file_handler = logging.FileHandler('/home/borisb/projects/llama/gol/sentiment.log')
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
-data_dir = ''
+data_dir = '/home/borisb/projects/llama/'
 
 class SentimentStrategy():
     """
@@ -36,20 +36,23 @@ class SentimentStrategy():
         sentiment_df = pd.read_csv(os.path.join(data_dir, 'sentiment_data.csv'))
         
         return sentiment_df
-    
+  
     def normalize_twitter_data(self, df: pd.DataFrame):
         logging.info("Normalizing data for twitter based usage")
-        
+      
         df['date'] = pd.to_datetime(df['date'])
         df = df.set_index(['date', 'symbol'])
 
         # Need to kind of normalize this so twitter likes + comments are included
         df['engagement_ratio'] = df['twitterComments']/df['twitterLikes']
         df = df[(df['twitterLikes']>20)&(df['twitterComments']>10)]
-        
+
         logging.debug("Done with normalizations")
-        
+
         return df
+
 sent_strat = SentimentStrategy()
 
 sentiment_df = sent_strat.load_data(data_dir)
+sentiment_df = sent_strat.normalize_twitter_data(sentiment_df)
+# logging.debug(sentiment_df)
