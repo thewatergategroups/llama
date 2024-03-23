2e379e89e82f1931da0fd013246330009974a1d3
Boris <bobiboychev@gmail.com>
2024-03-23 18:35:46 +0000

    Implement Nvidia testing strat

--- llama/test-strat.py
+++ llama/test-strat.py
@@ -6,74 +6,66 @@ from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame
 from alpaca.data.requests import StockBarsRequest
 from datetime import datetime
-from stocks.technical import GKV
+import llama.technical as tt
 
-def main():
-    print("Hello World!")
-    API_KEY='PKCTFA47WCR9XYAZJ6C8'
-    SECRET_KEY = 'jOHa3DTjOcOnezvKDoFjSbikcl2RDWCXwFg8PLF1'
+API_KEY=''
+SECRET_KEY = ''
 
-    trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 
-    # search for US equities
-    # search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
+# search for US equities
+# search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
 
-    # # assets = trading_client.get_all_assets(search_params)
-    # # print(assets)
+# # assets = trading_client.get_all_assets(search_params)
+# # print(assets)
 
-    # aapl_asset = trading_client.get_asset('AAPL')
+# aapl_asset = trading_client.get_asset('AAPL')
 
-    # if aapl_asset.tradable:
-    #     print('We can trade AAPL.')
+# if aapl_asset.tradable:
+#     print('We can trade AAPL.')
 
-    # print(aapl_asset)
-    # NVDA
-    stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+# print(aapl_asset)
+# NVDIA
+stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-    req_params = StockBarsRequest( 
-        symbol_or_symbols=["NVDA"],
-        timeframe=TimeFrame.Day,
-        start=datetime(2022, 9, 1),
-        end=datetime(2023, 9, 7)
-        )
+nvidia_full_df_req = StockBarsRequest( 
+    symbol_or_symbols=["NVDA"],
+    timeframe=TimeFrame.Day,
+    start=datetime(2022, 9, 1),
+    end=datetime(2023, 9, 7)
+    )
 
-    nvidia_df= stock_client.get_stock_bars(req_params)
+nvidia_adj_df_req = StockBarsRequest(
+    symbol_or_symbols=["NVDA"],
+    timeframe=TimeFrame.Day,
+    start=datetime(2022, 9, 1),
+    end=datetime(2023, 9, 7),
+    adjustment='split'
+    )
 
-    print(nvidia_df.df)
-    print("===========================================================================================================================")
-    print("===========================================================================================================================")
+nvidia_df = stock_client.get_stock_bars(nvidia_full_df_req)
+nvidia_adj_df= stock_client.get_stock_bars(nvidia_adj_df_req)
 
-    gvk_client = GKV()
+# nvidia_df= stock_client.get_stock_bars(nvidia_full_df_req)
+working_df = nvidia_df.df
 
-    df = nvidia_df.df
-    df_gkl = gvk_client.calculate_garman_klass_vol(df)
+# print(nvidia_df.df)
+# print(nvidia_adj_df.df['close'])
+# nvidia_df.df['adj close'] =  nvidia_df.df + nvidia_adj_df.df['close']
+working_df['adj close'] = nvidia_adj_df.df['close'].to_numpy()
+# nvidia_adj_df. = nvidia_adj_df.df.join(nvidia_adj_df.df['close'])
+print("===========================================================================================================================")
+print("===========================================================================================================================")
+print(working_df)
 
-    print(df_gkl)
+gvk_client = tt.GKV()
+working_df = gvk_client.calculate_garman_klass_vol(working_df)
+working_df = gvk_client.calculate_rsi_indicator(working_df)
+print("RSI::")
+# print(working_df)
+working_df = gvk_client.calculate_bollinger_bands(working_df)
+# working_df['atr'] = working_df.groupby(level=0, group_keys=False).apply(gvk_client.calculate_atr) # ValueError: Cannot set a DataFrame with multiple columns to the single column atr
+working_df['macd'] = working_df.groupby(level=0, group_keys=False)['adj close'].apply(gvk_client.calculate_macd)
+working_df['dollar_volume'] =  (working_df['adj close'] * working_df['volume'])/1e6
 
-
-    # print(nvidia_df)
-    # for symbol in symbols:
-    #     times = self.identify_missing_bars(symbol, time_frame, start_time, end_time)
-    #     for starttime, endtime in times:
-    #         print(
-    #             "getting bars for %s between %s and %s with timeframe of %s",
-    #             symbol,
-    #             starttime,
-    #             endtime,
-    #             time_frame.value,
-    #         )
-    #         request_params = StockBarsRequest(
-    #             symbol_or_symbols=[symbol],
-    #             timeframe=time_frame,
-    #             start=starttime.isoformat(),
-    #             end=endtime.isoformat(),
-    #         )
-    #         bars: BarSet = self.client.get_stock_bars(request_params)
-    #         # bars = self.fill_bars(bars, start_time, end_time)
-    #         if bars.data:
-    #             self.insert_bars(bars, time_frame)
-
-
-
-if __name__ == "__main__":
-    main()
+print(working_df)
