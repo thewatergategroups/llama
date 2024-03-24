2fdec8a99a426953c17948ab447d72db04126976
Boris <bobiboychev@gmail.com>
2024-03-24 11:57:48 +0000

    add sentiments

--- llama/indicators/sentiment.py
+++ llama/indicators/sentiment.py
@@ -11,22 +11,23 @@ yf.pdr_override()  # Enable caching
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(formatter)
 stdout_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-file_handler = logging.FileHandler('/home/borisb/projects/llama/gol/sentiment.log')
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+file_handler = logging.FileHandler("/home/borisb/projects/llama/gol/sentiment.log")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 logger.addHandler(stdout_handler)
 
-class SentimentStrategy():
+
+class SentimentStrategy:
     def __init__(self):
-        self.data_dir_twitter = '/home/borisb/projects/llama/'
+        self.data_dir_twitter = "/home/borisb/projects/llama/"
 
     def load_data(self, data_dir):
         """
@@ -38,11 +39,13 @@ class SentimentStrategy():
         Returns:
             _type_: returns a DataFrame from the data dir
         """
-        df = pd.read_csv(os.path.join(data_dir, 'sentiment_data.csv'))
+        df = pd.read_csv(os.path.join(data_dir, "sentiment_data.csv"))
 
         return df
 
-    def download_data(self, ticker_list, start_date: str, end_date: str) -> pd.DataFrame:
+    def download_data(
+        self, ticker_list, start_date: str, end_date: str
+    ) -> pd.DataFrame:
         """
         Uses Yahoo Finance to fetch a list of stocks and loads it to a DataFrame
 
@@ -55,9 +58,7 @@ class SentimentStrategy():
             pd.DataFrame: _description_
         """
         logger.info("Downloading data from Yahoo finance")
-        df = yf.download(tickers=ticker_list,
-                     start=start_date,
-                     end=end_date)
+        df = yf.download(tickers=ticker_list, start=start_date, end=end_date)
 
         return df
 
@@ -67,14 +68,16 @@ class SentimentStrategy():
         """
         logger.info("Normalizing data for twitter based usage")
 
-        df['date'] = pd.to_datetime(df['date'])
-        df = df.set_index(['date', 'symbol'])
+        df["date"] = pd.to_datetime(df["date"])
+        df = df.set_index(["date", "symbol"])
 
-        df['engagement_ratio'] = df['twitterComments'] / df['twitterLikes']
+        df["engagement_ratio"] = df["twitterComments"] / df["twitterLikes"]
 
         min_likes = 20
         min_comments = 10
-        df = df[(df['twitterLikes'] > min_likes) & (df['twitterComments'] > min_comments)]
+        df = df[
+            (df["twitterLikes"] > min_likes) & (df["twitterComments"] > min_comments)
+        ]
 
         logger.debug("Done with normalizations")
         return df
@@ -90,15 +93,20 @@ class SentimentStrategy():
             pd.DataFrame: _description_
         """
         logger.info("Starting to aggregate monthly data")
-        aggregated_df = (df.reset_index('symbol').groupby([pd.Grouper(freq='M'), 'symbol'])
-                 [['engagement_ratio']].mean())
+        aggregated_df = (
+            df.reset_index("symbol")
+            .groupby([pd.Grouper(freq="M"), "symbol"])[["engagement_ratio"]]
+            .mean()
+        )
 
-        aggregated_df['rank'] = (aggregated_df.groupby(level=0)['engagement_ratio' ].transform(lambda x: x.rank(ascending=False)))
+        aggregated_df["rank"] = aggregated_df.groupby(level=0)[
+            "engagement_ratio"
+        ].transform(lambda x: x.rank(ascending=False))
 
         logger.debug("Done with all aggregations")
         return aggregated_df
 
-    def select_top_stocks_monthly(self, df: pd.DataFrame, max_rank = 5) -> pd.DataFrame:
+    def select_top_stocks_monthly(self, df: pd.DataFrame, max_rank=5) -> pd.DataFrame:
         """
         Select the top N(5) stocks by rank for each month and fix the date to start at beginning of next month.
 
@@ -111,12 +119,12 @@ class SentimentStrategy():
         """
         logger.info("Starting to select top stocks for each month")
 
-        filtered_df = df[df['rank' ] <= max_rank].copy()
+        filtered_df = df[df["rank"] <= max_rank].copy()
         filtered_df = filtered_df.reset_index(level=1)
 
         # Set the beginning of the next month (!!)
         filtered_df.index = filtered_df.index + pd.DateOffset(1)
-        filtered_df = filtered_df.reset_index().set_index(['date', 'symbol'])
+        filtered_df = filtered_df.reset_index().set_index(["date", "symbol"])
         # logger.debug(filtered_df.head(20))
         logger.debug("Done with choosing the top 20 stocks")
         return filtered_df
@@ -127,11 +135,13 @@ class SentimentStrategy():
         Create a dictionary containing start of month and corresponded selected stocks.
         """
         logger.info("Filtering and selecting stocks for each month")
-        dates = df.index.get_level_values('date').unique().tolist()
+        dates = df.index.get_level_values("date").unique().tolist()
 
         top_number_of_stocks_with_dates = {}
         for d in dates:
-            top_number_of_stocks_with_dates[d.strftime('%Y-%m-%d')] = df.xs(d, level=0).index.tolist()
+            top_number_of_stocks_with_dates[d.strftime("%Y-%m-%d")] = df.xs(
+                d, level=0
+            ).index.tolist()
 
         logger.debug(top_number_of_stocks_with_dates)
         return top_number_of_stocks_with_dates
@@ -144,9 +154,15 @@ class SentimentStrategy():
         portfolio_df = pd.DataFrame()
 
         for start_date in dates_to_top_stocks.keys():
-            end_date = (pd.to_datetime(start_date) + pd.offsets.MonthEnd()).strftime('%Y-%m-%d')
+            end_date = (pd.to_datetime(start_date) + pd.offsets.MonthEnd()).strftime(
+                "%Y-%m-%d"
+            )
             cols = dates_to_top_stocks[start_date]
-            temp_df = returns_df[start_date:end_date][cols].mean(axis=1).to_frame('portfolio_return')
+            temp_df = (
+                returns_df[start_date:end_date][cols]
+                .mean(axis=1)
+                .to_frame("portfolio_return")
+            )
             portfolio_df = pd.concat([portfolio_df, temp_df], axis=0)
 
         logger.debug("Done with portfolio calculation")
@@ -161,14 +177,14 @@ class SentimentStrategy():
             df (pd.DataFrame): _description_
         """
         logger.info("Plotting")
-        plt.style.use('ggplot')
+        plt.style.use("ggplot")
 
         df.plot(figsize=(16, 6))
 
-        plt.title('Twitter Engagement Ratio Strategy Return Over Time')
+        plt.title("Twitter Engagement Ratio Strategy Return Over Time")
         plt.gca().yaxis.set_major_formatter(mtick.PercentFormatter(1))
 
-        plt.ylabel('Return')
+        plt.ylabel("Return")
 
         # plt.show()
         plt.savefig("returns.png")
@@ -177,12 +193,12 @@ class SentimentStrategy():
         """
         Twitter Sentiment Investing Strategy
         # 1. Load Twitter Sentiment Data
-        # Load the twitter sentiment dataset, set the index, calculate engagement ratio, 
+        # Load the twitter sentiment dataset, set the index, calculate engagement ratio,
         # and filter out stocks with no significant twitter activity.
         """
         logger.info("Starting twitter execute strategy")
-        START_DATE = '2021-01-01'
-        END_DATE = '2023-03-01'
+        START_DATE = "2021-01-01"
+        END_DATE = "2023-03-01"
 
         sentiment_df = self.load_data(self.data_dir_twitter)
         sentiment_df = self.normalize_twitter_data(sentiment_df)
@@ -194,16 +210,16 @@ class SentimentStrategy():
         dates_to_top_stocks = self.select_stocks_beginning_of_month(filtered_df)
 
         # Download fresh stock prices for only selected/shortlisted stocks
-        stocks_list = sentiment_df.index.get_level_values('symbol').unique().tolist()
+        stocks_list = sentiment_df.index.get_level_values("symbol").unique().tolist()
         prices_df = self.download_data(stocks_list, START_DATE, END_DATE)
         # Calculate Portfolio Returns with monthly rebalancing
-        returns_df = np.log(prices_df['Adj Close']).diff()
+        returns_df = np.log(prices_df["Adj Close"]).diff()
         portfolio_df = self.calculate_portfolio(returns_df, dates_to_top_stocks)
         # logger.debug(portfolio_df)
 
         # Download NASDAQ/QQQ prices and calculate returns to compare to our strategy
-        qqq_df = self.download_data('QQQ', START_DATE, END_DATE)
-        qqq_ref = np.log(qqq_df['Adj Close']).diff().to_frame('nasdaq_return')
+        qqq_df = self.download_data("QQQ", START_DATE, END_DATE)
+        qqq_ref = np.log(qqq_df["Adj Close"]).diff().to_frame("nasdaq_return")
 
         portfolio_df = portfolio_df.merge(qqq_ref, left_index=True, right_index=True)
 
@@ -212,6 +228,7 @@ class SentimentStrategy():
         portfolios_cumulative_return = np.exp(np.log1p(portfolio_df).cumsum()).sub(1)
 
         self.plot_df(portfolios_cumulative_return)
+
 
 sent_strat = SentimentStrategy()
 sent_strat.execute_twitter_sent_strategy()
