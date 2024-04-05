d42d8b6290a42dab4b33d006cc7714f635d49325
Boris Boychev <bobiboychev@gmail.com>
2024-04-05 22:27:09 +0100

    pass down default conditions

--- llama/strats/sample/conditions.py
+++ llama/strats/sample/conditions.py
@@ -18,6 +18,22 @@ def second_indicator(most_recent_bar: Bar, _: Trader) -> bool:
 
 
 def get_second_indicator():
+    """
+    swing trading system and that we will trade on a daily chart
+    we use simple moving averages to help us identify a new trend as early as possible.
+
+    The Stochastic helps us determine if it`s still ok for us to enter a trade after
+    a moving average crossover, and it also helps us avoid oversold and overbought areas.
+
+    The RSI is an extra confirmation tool that helps us determine the strength of our trend.
+    After figuring out our trade setup, we then determined our risk for each trade.
+
+    (For this system, we are willing to risk 100 pips on each trade) <= TODO: implement
+
+
+    Returns:
+        _type_: _description_
+    """
     return [
         Condition(
             name="first_indicator",
--- llama/strats/sample/start.py
+++ llama/strats/sample/start.py
@@ -5,14 +5,13 @@ https://www.babypips.com/learn/forex/the_so_easy-its-ridiculous-system
 
 from ..base import Strategy, get_base_conditions
 
-# from .conditions import get_vwap_conditions
+from .conditions import get_second_indicator
 
 
 class SEIR(Strategy):
     """Volume weighted average proce strategy class"""
 
-    DEFAULT_CONDITIONS = get_base_conditions()
-    # DEFAULT_CONDITIONS = get_vwap_conditions() + get_base_conditions()
+    DEFAULT_CONDITIONS = get_second_indicator() + get_base_conditions()
     NAME = "So Easy It's Ridiculous"
     ALIAS = "seir"
     ACTIVE = True
