47bfa3b79e4737d996953885bf9d3eed47c5a3c1
Boris Boychev <bobiboychev@gmail.com>
2024-04-05 23:32:52 +0100

    clean up

--- llama/strats/sample/conditions.py
+++ llama/strats/sample/conditions.py
@@ -22,55 +22,6 @@ class ExtedndedBar(Bar):
         self.sma_log: Optional[float]
 
 
-def first_indicator(most_recent_bar: Bar, _: Trader):
-    return True
-
-
-def second_indicator(most_recent_bar: Bar, _: Trader) -> bool:
-    return False
-
-
-def get_second_indicator():
-    """
-    swing trading system and that we will trade on a daily chart
-    we use simple moving averages to help us identify a new trend as early as possible.
-
-    The Stochastic helps us determine if it`s still ok for us to enter a trade after
-    a moving average crossover, and it also helps us avoid oversold and overbought areas.
-
-    The RSI is an extra confirmation tool that helps us determine the strength of our trend.
-    After figuring out our trade setup, we then determined our risk for each trade.
-
-    (For this system, we are willing to risk 100 pips on each trade) <= TODO: implement
-
-
-    Returns:
-        _type_: _description_
-    """
-    return [
-        Condition(
-            name="first_indicator",
-            func=first_indicator,
-            variables={"param": 0},
-            active=True,
-            side=OrderSide.BUY,
-            type=ConditionType.AND,
-        ),
-        Condition(
-            name="second_indicator",
-            func=second_indicator,
-            variables={},
-            active=True,
-            side=OrderSide.BUY,
-            type=ConditionType.AND,
-        ),
-    ]
-
-
-# Assuming utility functions exist:
-# calculate_sma_short, calculate_sma_long, calculate_stochastic, calculate_rsi
-
-
 def sma_crossover_buy(most_recent_bar: ExtedndedBar, _: Trader) -> bool:
     """Condition for SMA crossover buy signa."""
     short_sma = most_recent_bar["sma_short"]
@@ -91,7 +42,20 @@ def rsi_below_threshold(most_recent_bar: ExtedndedBar, _: Trader) -> bool:
 
 
 def get_extended_conditions():
-    """Get conditions integrating SMA, Stochastic, and RSI."""
+    """
+    Get conditions integrating SMA, Stochastic, and RSI
+
+    swing trading system and that we will trade on a daily chart
+    we use simple moving averages to help us identify a new trend as early as possible.
+
+    The Stochastic helps us determine if it`s still ok for us to enter a trade after
+    a moving average crossover, and it also helps us avoid oversold and overbought areas.
+
+    The RSI is an extra confirmation tool that helps us determine the strength of our trend.
+    After figuring out our trade setup, we then determined our risk for each trade.
+
+    (For this system, we are willing to risk 100 pips on each trade) <= TODO: implement
+    """
     return [
         Condition(
             name="sma_crossover_buy_signal",
